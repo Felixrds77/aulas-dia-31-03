5b64c76c230be63ae665a00994be205ba36bb108
Felixrds77 <felixcanarinho@gmail.com>
2025-03-31 11:22:12 -0300

    meida com if, elif e else

--- exercicio05.py
+++ exercicio05.py
@@ -1,7 +1,8 @@
-nota01 = float(input("digite a primeira nota:"))
-nota02 = float(input("digite a segunda nota:"))
-nota03 = float(input("digite a terceira nota:"))
+nota01 = float(input("digite a primeira nota: "))
+nota02 = float(input("digite a segunda nota: "))
+nota03 = float(input("digite a terceira nota: "))
 soma = (nota01+nota02+nota03)/3
 print(f"sua nota é {soma}" )
 if soma>=7: print("você está aprovado")
-else:print("você está reprovado")
+elif soma>=4 : print("você esta em recuperação")
+else :print("você está reprovado")
