bfaed398bcbe86377078ca0822ee3d686b9f87f9
Felixrds77 <felixcanarinho@gmail.com>
2025-03-31 08:40:25 -0300

    atividade01

---
+++ exercico01.py
@@ -0,0 +1,4 @@
+nome = input("coloque seu nome: ")
+idade = int(input("coloque sua idade: "))
+salario = int(input("coloque seu salario: "))
+print(f"seu  nome é: {nome}, sua idade é: {idade} e o seu salario é: {salario} R$")
