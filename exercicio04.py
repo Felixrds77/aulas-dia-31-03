d51c47967eda66dbc42d440599147e88ee0cbe2a
Felixrds77 <felixcanarinho@gmail.com>
2025-03-31 11:04:02 -0300

    atividade de notas if e else

---
+++ exercicio04.py
@@ -0,0 +1,4 @@
+num1 = int(input("insira um numero: "))
+num2 = int(input("insira um numero: "))
+if num1>num2: print(num2,num1)
+else: print(num1,num2)
---
+++ exercicio05.py
@@ -0,0 +1,7 @@
+nota01 = float(input("digite a primeira nota:"))
+nota02 = float(input("digite a segunda nota:"))
+nota03 = float(input("digite a terceira nota:"))
+soma = (nota01+nota02+nota03)/3
+print(f"sua nota é {soma}" )
+if soma>=7: print("você está aprovado")
+else:print("você está reprovado")
