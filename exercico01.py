18c2099af480429018b2dfa5e1e80b956632846e
Felixrds77 <felixcanarinho@gmail.com>
2025-03-31 09:04:22 -0300

    percental do salario

--- exercico01.py
+++ exercico01.py
@@ -1,4 +1,8 @@
 nome = input("coloque seu nome: ")
 idade = int(input("coloque sua idade: "))
-salario = int(input("coloque seu salario: "))
-print(f"seu  nome é: {nome}, sua idade é: {idade} e o seu salario é: {salario} R$")
+salario = float(input("coloque seu salario: "))
+print(f"seu  nome é: {nome}, sua idade é: {idade} e o seu salario é: {salario:.2f}  ")
+percentual = float(input("informe o percentual do almento: "))
+salario +=(salario*percentual)/100
+print(f"seu novo salario é : {salario}")
+
