91dcac431b337efcdefd117f3b1e86f52481b730
Felixrds77 <felixcanarinho@gmail.com>
2025-03-31 09:42:15 -0300

    as 4 operações

---
+++ exercicio02.py
@@ -0,0 +1,12 @@
+num1 = float(input("digite um numero: "))
+num2 = float(input("digite um numero: "))
+
+soma = (num1+num2)
+multiplicacao = (num1*num2)
+divisao = (num1/num2)
+subtracao = (num1-num2)
+
+print(f"{num1} + {num2} = {soma}")
+print(f"{num1} * {num2} = {multiplicacao}")
+print(f"{num1} / {num2} = {divisao}")
+print(f"{num1} - {num2} = {subtracao}")
