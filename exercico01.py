6e3b78a09416ee453b8ed354fc9523e896cafb38
Felixrds77 <felixcanarinho@gmail.com>
2025-03-31 09:16:16 -0300

    valor do salario atual

--- exercico01.py
+++ exercico01.py
@@ -3,6 +3,7 @@ idade = int(input("coloque sua idade: "))
 salario = float(input("coloque seu salario: "))
 print(f"seu  nome é: {nome}, sua idade é: {idade} e o seu salario é: {salario:.2f}  ")
 percentual = float(input("informe o percentual do almento: "))
-salario +=(salario*percentual)/100
-print(f"seu novo salario é : {salario}")
+aumentosalario =salario*(percentual/100)
+valor = salario + aumentosalario
+print(f"seu novo salario é : {valor:.2f}, seu aumento foi de {aumentosalario:.2f} ")
 
